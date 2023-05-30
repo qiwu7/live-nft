3b12498447fcf5b20d022694fe4f7fc8a61a007e
Qi Wu <qi.wu@coinbase.com>
2023-05-30 19:47:07 +0800

    Create NFT contract

---
+++ ethereum/contracts/LiveNFT.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract LiveNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("LiveNFT", "LNFT") {}
+
+    function mint(address holder)
+        public
+        returns (uint256)
+    {
+        uint256 newItemId = _tokenIds.current();
+        _mint(holder, newItemId);
+
+        string memory tokenURI = string(abi.encodePacked(
+            "https://live-nft.herokuapp.com/address/",
+            holder,
+            "/metadata"
+        ));
+
+        _setTokenURI(newItemId, tokenURI);
+
+        _tokenIds.increment();
+        return newItemId;
+    }
+}
